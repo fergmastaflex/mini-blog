54698f52079d64576b6c726f6097ab53a97f9ffc
DevPoint 2 <DevPoint2@DevPoints-iMac-5.local>
2013-09-27 16:08:23 -0600

    Initial setup of rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniBlog::Application.config.secret_token = '6e7c0fd33c88eb4cb2ac93880a937381ae490c342864b7ae0841bc1ab3e26f2dd5b2740e52a0dbe7a545d25b4174b88ca3394d20f2de6d7b70e82e0dd418cb38'

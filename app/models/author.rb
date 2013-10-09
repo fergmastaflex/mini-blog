a6c3c23e0cf2a2364be29934a7b332c3f40dc2e4
ferg <joey@fergmedia.com>
2013-10-08 18:28:22 -0600

    Added password authentication to Authors. added signup to navigation. Added CSS to global navigation.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,13 @@
 class Author < ActiveRecord::Base
+  has_secure_password
 	has_many :posts
-  	attr_accessible :bio, :email, :first_name, :last_name, :twitter, :username
+  has_many :comments, through: :posts
+  	attr_accessible :bio, :email, :first_name, :last_name, :twitter, :username, :password, :password_confirmation
   	# presence
   	validates :email, :first_name, :last_name, :username, presence: true
   	# uniqueness
   	validates :email, :twitter, :username, uniqueness: true, allow_blank: true
   	# format
-  	validates :bio, length: {in: 30..500}
+  	validates :bio, length: {in: 5..500}
   	validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 end
